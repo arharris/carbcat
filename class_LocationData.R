c4c2771523b6b640653fcbfb9d8b0dc0419afb29
arharris <arh30@humboldt.edu>
2018-05-25 12:58:18 -0700

    created object class to store location data

---
+++ class_LocationData.R
@@ -0,0 +1,22 @@
+# ==========================================================================================
+# AUTHOR: Andrew Harris
+#	  Schatz Energy Research Center
+#	  Humboldt State University
+# ==========================================================================================
+# VERSION:
+#   2018-05-25 Initial Development
+# ==========================================================================================
+# This defines an object class to store location information highlighted from GIS
+# applications that will be used for the CARBCAT model. However we interface GIS to R, the 
+# relevant information is stored in this "location.data" object class, to ensure a single 
+# uniform data structure throighout CARBCAT.
+# ==========================================================================================
+setClass("location.data",
+         slots=c(
+           land.ownership = "string",
+           size.class.decay.rates = "data.frame",
+           slope = "numeric",
+           biomass.market.volume = "string",
+           pulp.market = "logical"
+         )
+        )
